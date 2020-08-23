2cf549745dcd0ed4150747b3673d15575d23dc8f
Sankalp <sankalp2405@outlook.com>
2020-08-23 18:35:02 +0530

    Programming Assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions very similar to that given in the example of this assignment.
+## Instead of finding the mean of a numerical vector, we find the 
+## inverse of a matrix
+## We cache potentially time consuming methods of getting and setting both the 
+## matrix and it's inverse
 
-## Write a short comment describing this function
+## In the first function we do the caching
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(1:9, 3, 3)) {
+        
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached result")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
